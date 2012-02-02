65e5e9c63c3028cf37bfd721134d29167d998972
vflima06 <vflima06@513e52e1-5798-1d8d-9f99-0c8dabf8607d>
2012-02-02 08:32:55 +0000

    ** BUGs DE MAPA ** BUGS EM PALLET CONSERTADOS UNIQUE IDS REPETIDOS, REMOVIDOS ** SCRIPT LAB CONSERTADO** AGORA SÓ É POSSIVEL SAIR DO LAB, APÓS FAZER A QUEST DO POKEMON INICIAL. ** FALA QUEST DOS POKE ** AJEITADO DIALOG DA QUEST DO POKEMON, AGORA ELA DIZ QUAL POKEMON VOCÊ ESCOLHEU ( EM INGLÊS) ** PASTA ACTIONS ORGANIZADA ** ORGANIZADA PASTA ACTIONS COM NOVAS PASTAS PARA SEREM COLOCADAS RESPECTIVAS QUESTS E SISTEMAS ** SISTEMA LIMPO ** !LEAVE DUPLICADO CONSERTADO !LEAVESAFARI NOVO COMANDO, PARA SAIR DA SAFARI. ERROR RESPECTIVOS DE ACTIONS CONSERTADOS RETIRADO TODOS OS TALKACTIONS !VIP PARA EVITAR ERROS NA INICIAÇÃO DO SERIVOR

--- Servidor/Pokemon EX 2.0/data/movements/scripts/oaklab.lua
+++ Servidor/Pokemon EX 2.0/data/movements/scripts/oaklab.lua
@@ -3,15 +3,17 @@ function onStepIn(cid, item, position, fromPosition)
 local tileConfig = {
         kickPos = fromPosition,
         kickEffect = CONST_ME_POFF,
-        kickMsg = "I think its a better idea talk to the profesor first",
+        kickMsg = "You have to talk with the professor before leave the lab.",
         enterEffect = CONST_ME_MAGIC_BLUE,
 }
 
-    if getPlayerStorageValue(cid, 54842) ~= 0 then
+    if getPlayerStorageValue(cid, 54842) ~=1  then
         doTeleportThing(cid, tileConfig.kickPos)
         doSendMagicEffect(tileConfig.kickPos, tileConfig.kickEffect)
+        doPlayerSendTextMessage(cid,22,"Head up stairs and talk to professor.")
         doPlayerSendCancel(cid, tileConfig.kickMsg)
-        return
-    end
+else
+end
         return true
+
 end
